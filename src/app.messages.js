08df8db49923e68b83a2167e628358c2fe44371f
vnarah2 <vnarah@illinois.edu>
2018-12-07 15:07:27 -0600

    FD-100 Added welcome text

--- src/app.messages.js
+++ src/app.messages.js
@@ -6,10 +6,17 @@ export const userNotActiveMessage = "Registration is successful, but your accoun
 export const invalidLoginCredentials = "We do not recognize your username and/or password. Please try again.";
 export const dataWolfGetTokenCallFailed = "An internal error occurred while logging in. Please try again later.";
 
-export const welcometext = ["The objective of this project is to provide a web-based farm program analysis tool to assist farmers, " +
-"academics, and policymakers to understand programs and policies surrounding the Farm Bill.","  "," Include a dashboard that visualizes options such as mapping payments at county, " +
-
-"regional (including Congressional district), state and national levels.", "  ", " Funding for this project has been provided by ACES, U of I"
+export const welcometext = ["The farmdoc Farm Program project provides a web-based farm program analysis tool to support farmer decisions," +
+" as well as assist an understanding of the programs by farmers, academics and policymakers. Farm programs have been reauthorized by" +
+" Congress in the 2018 Farm Bill and this tool provides a web-based dashboard that can generate estimated program payment for the commodities" +
+" covered by the programs (e.g., corn, wheat and soybeans). The dashboard provides program payment estimates for the individual farm using " +
+"county level historical data and modeled price and yield forecasts. It will also produce detailed visualization options, such as mapping payments" +
+" at county, regional (including Congressional district), state and national levels. Currently, the tool supports corn, wheat and soybeans by" +
+" mapping estimated program payments for ARC (Agriculture Risk Coverage) and PLC (Price Loss Coverage) as reauthorized by Congress.",
+	"  ",
+	" Funding for this project has been provided by the Gardner Agricultural Policy Program (GAPP) in the Department of Agricultural and Consumer Economics" +
+	" (ACE) at the University of Illinois at Urbana-Champaign and by a cooperative agreement with the Office of the Chief Economist" +
+	" at the U.S. Department of Agriculture."
 ];
 export const register = "Creating an account allows you to save the simulation data" +
 	" and information about your farm and fields.";
