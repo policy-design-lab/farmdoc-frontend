99e1160cefe6bcc05d502ec69ae79dc80735c2f3
vnarah2 <vnarah2@illinois.edu>
2020-02-21 22:25:09 -0600

    FD-262: Removed an unused function

--- src/actions/insPremiums.js
+++ src/actions/insPremiums.js
@@ -8,9 +8,3 @@ export const handleCountyProductsResults = (countyProductsResults) => ({
 	type: "ADD_COUNTY_RESULT",
 	countyProductsResults
 });
-
-export const changeCropCode = (cropCode) => ({
-	type: "CHANGE_CROPCODE",
-	cropCode
-});
-
