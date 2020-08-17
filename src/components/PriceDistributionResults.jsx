09a3b556f2f014396a018e2fd5ec0b08e0636a30
MO <ondrejce@illinois.edu>
2020-08-17 00:50:17 -0500

    add mu sigma effective calculation, height formatting

--- src/components/PriceDistributionResults.jsx
+++ src/components/PriceDistributionResults.jsx
@@ -20,6 +20,10 @@ import {
 	generateProbPoints,
 	regeneratePriceTableData
 } from "../public/pd_data";
+import {
+	calculateMuSigmaEff
+} from "../public/pd_sigmamu";
+
 import {roundResults} from "../public/utils";
 
 const styles = theme => ({
@@ -95,6 +99,7 @@ const prepareProbChart = (poi, chartData, title, yAxisLabel, dataColumn) => {
 					}
 				}
 			},
+			maintainAspectRatio: false,
 			scales: {
 				yAxes: [{
 					ticks: {
@@ -202,6 +207,9 @@ class PriceDistributionResults extends Component {
 
 	render() {
 		const {classes} = this.props;
+		const tabulator_options = {
+			height: 250,
+		};
 
 		let futuresCode = "";
 		let resultData = null;
@@ -212,7 +220,7 @@ class PriceDistributionResults extends Component {
 		let futuresData = null;
 		let dte = null;
 		let optionValuesByStrike = null;
-		let previousSolution = null;
+		let solution = null;
 
 		let priceTableData = null;
 		let probTableData = null;
@@ -224,9 +232,9 @@ class PriceDistributionResults extends Component {
 		if (this.props.hasOwnProperty("pdResults") && this.props["pdResults"] !== null) {
 			resultData = this.props["pdResults"];
 		}
-		else {
-			resultData = "{\"ZCZ21\": {\"results\": {\"price\": 3.655, \"optionValuesByStrike\": [{\"strike\": 310, \"call\": 62.625, \"put\": 5.375}, {\"strike\": 320, \"call\": 54.625, \"put\": 7.375}, {\"strike\": 330, \"call\": 47.125, \"put\": 9.875}, {\"strike\": 340, \"call\": 40.25, \"put\": 13.0}, {\"strike\": 350, \"call\": 34.125, \"put\": 17.25}, {\"strike\": 360, \"call\": 28.75, \"put\": 21.5}, {\"strike\": 370, \"call\": 24.125, \"put\": 26.875}, {\"strike\": 380, \"call\": 20.125, \"put\": 32.875}, {\"strike\": 390, \"call\": 16.75, \"put\": 39.5}, {\"strike\": 400, \"call\": 13.875, \"put\": 46.625}, {\"strike\": 410, \"call\": 11.5, \"put\": 54.25}, {\"strike\": 420, \"call\": 9.5, \"put\": 62.25}, {\"strike\": 430, \"call\": 7.875, \"put\": 70.5}], \"dte\": -487, \"futuresData\": {\"dte\": -487, \"data\": {\"180-0C\": 187.375, \"180-0P\": 0.125, \"190-0C\": 177.375, \"190-0P\": 0.125, \"200-0C\": 167.375, \"200-0P\": 0.125, \"210-0C\": 157.5, \"210-0P\": 0.25, \"220-0C\": 147.625, \"220-0P\": 0.375, \"230-0C\": 137.75, \"230-0P\": 0.5, \"240-0C\": 127.875, \"240-0P\": 0.625, \"250-0C\": 118.125, \"250-0P\": 0.875, \"260-0C\": 108.375, \"260-0P\": 1.125, \"270-0C\": 98.75, \"270-0P\": 1.5, \"280-0C\": 89.375, \"280-0P\": 2.125, \"290-0C\": 80.125, \"290-0P\": 2.875, \"300-0C\": 71.125, \"300-0P\": 4.0, \"310-0C\": 62.625, \"310-0P\": 5.375, \"320-0C\": 54.625, \"320-0P\": 7.375, \"330-0C\": 47.125, \"330-0P\": 9.875, \"340-0C\": 40.25, \"340-0P\": 13.0, \"350-0C\": 34.125, \"350-0P\": 17.25, \"360-0C\": 28.75, \"360-0P\": 21.5, \"370-0C\": 24.125, \"370-0P\": 26.875, \"380-0C\": 20.125, \"380-0P\": 32.875, \"390-0C\": 16.75, \"390-0P\": 39.5, \"400-0C\": 13.875, \"400-0P\": 46.625, \"410-0C\": 11.5, \"410-0P\": 54.25, \"420-0C\": 9.5, \"420-0P\": 62.25, \"430-0C\": 7.875, \"430-0P\": 70.5, \"440-0C\": 6.5, \"440-0P\": 79.25, \"450-0C\": 5.375, \"450-0P\": 88.125, \"460-0C\": 4.5, \"460-0P\": 97.25, \"470-0C\": 3.75, \"470-0P\": 106.5, \"480-0C\": 3.25, \"480-0P\": 116.0, \"490-0C\": 2.875, \"490-0P\": 125.5, \"500-0C\": 2.5, \"500-0P\": 135.125, \"510-0C\": 2.125, \"510-0P\": 144.875, \"520-0C\": 2.0, \"520-0P\": 154.625, \"530-0C\": 1.75, \"530-0P\": 164.5, \"540-0C\": 1.625, \"540-0P\": 174.25, \"550-0C\": 1.5, \"550-0P\": 184.125, \"560-0C\": 1.375, \"560-0P\": 194.0, \"570-0C\": 1.25, \"570-0P\": 203.875, \"580-0C\": 1.125, \"580-0P\": 213.875, \"590-0C\": 1.0, \"590-0P\": 223.75, \"600-0C\": 1.0, \"600-0P\": 233.625, \"610-0C\": 0.875, \"610-0P\": 243.625, \"620-0C\": 0.75, \"620-0P\": 253.5, \"630-0C\": 0.75, \"630-0P\": 263.5}, \"price\": 365.5}, \"previousSolution\": {\"sigma\": 0.2108, \"mu\": 1.3173}}}}";
-		}
+		// else {
+		// 	resultData = "{\"ZCZ21\": {\"results\": {\"price\": 3.655, \"optionValuesByStrike\": [{\"strike\": 310, \"call\": 62.625, \"put\": 5.375}, {\"strike\": 320, \"call\": 54.625, \"put\": 7.375}, {\"strike\": 330, \"call\": 47.125, \"put\": 9.875}, {\"strike\": 340, \"call\": 40.25, \"put\": 13.0}, {\"strike\": 350, \"call\": 34.125, \"put\": 17.25}, {\"strike\": 360, \"call\": 28.75, \"put\": 21.5}, {\"strike\": 370, \"call\": 24.125, \"put\": 26.875}, {\"strike\": 380, \"call\": 20.125, \"put\": 32.875}, {\"strike\": 390, \"call\": 16.75, \"put\": 39.5}, {\"strike\": 400, \"call\": 13.875, \"put\": 46.625}, {\"strike\": 410, \"call\": 11.5, \"put\": 54.25}, {\"strike\": 420, \"call\": 9.5, \"put\": 62.25}, {\"strike\": 430, \"call\": 7.875, \"put\": 70.5}], \"dte\": -487, \"futuresData\": {\"dte\": -487, \"data\": {\"180-0C\": 187.375, \"180-0P\": 0.125, \"190-0C\": 177.375, \"190-0P\": 0.125, \"200-0C\": 167.375, \"200-0P\": 0.125, \"210-0C\": 157.5, \"210-0P\": 0.25, \"220-0C\": 147.625, \"220-0P\": 0.375, \"230-0C\": 137.75, \"230-0P\": 0.5, \"240-0C\": 127.875, \"240-0P\": 0.625, \"250-0C\": 118.125, \"250-0P\": 0.875, \"260-0C\": 108.375, \"260-0P\": 1.125, \"270-0C\": 98.75, \"270-0P\": 1.5, \"280-0C\": 89.375, \"280-0P\": 2.125, \"290-0C\": 80.125, \"290-0P\": 2.875, \"300-0C\": 71.125, \"300-0P\": 4.0, \"310-0C\": 62.625, \"310-0P\": 5.375, \"320-0C\": 54.625, \"320-0P\": 7.375, \"330-0C\": 47.125, \"330-0P\": 9.875, \"340-0C\": 40.25, \"340-0P\": 13.0, \"350-0C\": 34.125, \"350-0P\": 17.25, \"360-0C\": 28.75, \"360-0P\": 21.5, \"370-0C\": 24.125, \"370-0P\": 26.875, \"380-0C\": 20.125, \"380-0P\": 32.875, \"390-0C\": 16.75, \"390-0P\": 39.5, \"400-0C\": 13.875, \"400-0P\": 46.625, \"410-0C\": 11.5, \"410-0P\": 54.25, \"420-0C\": 9.5, \"420-0P\": 62.25, \"430-0C\": 7.875, \"430-0P\": 70.5, \"440-0C\": 6.5, \"440-0P\": 79.25, \"450-0C\": 5.375, \"450-0P\": 88.125, \"460-0C\": 4.5, \"460-0P\": 97.25, \"470-0C\": 3.75, \"470-0P\": 106.5, \"480-0C\": 3.25, \"480-0P\": 116.0, \"490-0C\": 2.875, \"490-0P\": 125.5, \"500-0C\": 2.5, \"500-0P\": 135.125, \"510-0C\": 2.125, \"510-0P\": 144.875, \"520-0C\": 2.0, \"520-0P\": 154.625, \"530-0C\": 1.75, \"530-0P\": 164.5, \"540-0C\": 1.625, \"540-0P\": 174.25, \"550-0C\": 1.5, \"550-0P\": 184.125, \"560-0C\": 1.375, \"560-0P\": 194.0, \"570-0C\": 1.25, \"570-0P\": 203.875, \"580-0C\": 1.125, \"580-0P\": 213.875, \"590-0C\": 1.0, \"590-0P\": 223.75, \"600-0C\": 1.0, \"600-0P\": 233.625, \"610-0C\": 0.875, \"610-0P\": 243.625, \"620-0C\": 0.75, \"620-0P\": 253.5, \"630-0C\": 0.75, \"630-0P\": 263.5}, \"price\": 365.5}, \"previousSolution\": {\"sigma\": 0.2108, \"mu\": 1.3173}}}}";
+		// }
 		// 	"ZCZ21": 	"results": 	"price": 3.655,
 		// 							"optionValuesByStrike": [{}, {}, {}, {}, {}],
 		// 							"dte": -487,
@@ -253,12 +261,16 @@ class PriceDistributionResults extends Component {
 				results = pdResultsObj[futuresCode]["results"];
 
 				price = results["price"];
-				optionValuesByStrike = results["optionValuesByStrike"];
-				dte = results["dte"];
-				futuresData = results["futuresData"];
-				previousSolution = results["previousSolution"];
+				// optionValuesByStrike = results["optionValuesByStrike"];
+				// dte = results["dte"];
+				// futuresData = results["futuresData"];
+				// solution = results["previousSolution"];
 
-				const chartData = generateChartData(previousSolution.sigma, previousSolution.mu);
+				// this should be calculated on the server side
+				solution = calculateMuSigmaEff(results);
+				//console.log(solution);
+
+				const chartData = generateChartData(solution.sigma, solution.mu);
 				//console.log(chartData);
 
 				priceOfInterest = price;
@@ -269,8 +281,8 @@ class PriceDistributionResults extends Component {
 				graph2 = prepareProbChart(priceOfInterest, chartData, "Probability of Prices at Expiration", "Relative Probability", "litPyt");
 				// console.log(graph1.data);
 
-				priceTableData = regeneratePriceTableData(price, previousSolution.sigma, previousSolution.mu);
-				probTableData = generateProbPoints(previousSolution.sigma, previousSolution.mu);
+				priceTableData = regeneratePriceTableData(price, solution.sigma, solution.mu);
+				probTableData = generateProbPoints(solution.sigma, solution.mu);
 				//console.log(probTableData);
 				table1 = prepareTable(priceTableData, "Price at", "Expiration ($)", "Below (%)", "price", "probability", "table1");
 				table2 = prepareTable(probTableData, "At expiration", "Below (%)", "Price ($)", "percentile", "price", "table2");
@@ -283,7 +295,7 @@ class PriceDistributionResults extends Component {
 						<Grid item xs={10}>
 							{/*<div style={{padding: 0, display: "inline-block"}} >*/}
 							{/*	{futuresCode}<br />*/}
-							{/*	{JSON.stringify(previousSolution)}*/}
+							{/*	{JSON.stringify(solution)}*/}
 							{/*</div>*/}
 							<div style={{textAlign: "left", fontSize: "1.0em", fontWeight: 500, maxWidth: "1085px"}}>
 								The charts below show the corn price distribution at expiration in two related forms.
@@ -299,14 +311,14 @@ class PriceDistributionResults extends Component {
 					<Grid container>
 						<Grid item xs={1} />
 						<Grid item xs={7}>
-							<div style={{width: "90%", margin: "auto", padding: "10px"}}>
+							<div style={{width: "90%", margin: "auto", height: "300px", padding: "10px"}}>
 								<Line data={graph1.data} legend={graph1.legend} options={graph1.options}/>
 							</div>
 						</Grid>
 						<Grid item xs={3}>
-							<div style={{width: "100%", marginTop: "30px", padding: "10px"}}>
+							<div style={{width: "100%", marginTop: "20px", padding: "10px"}}>
 								<ReactTabulator
-									data={table1.data} layout={"fitColumns"} columns={table1.columns}
+									data={table1.data} layout={"fitColumns"} columns={table1.columns} options={tabulator_options}
 									rowClick={table1.rowClick} rowFormatter={table1.rowFormatter} tooltips={true}
 								/>
 							</div>
@@ -337,14 +349,14 @@ class PriceDistributionResults extends Component {
 					<Grid container>
 						<Grid item xs={1} />
 						<Grid item xs={7}>
-							<div style={{width: "90%", margin: "auto", padding: "10px"}}>
+							<div style={{width: "90%", margin: "auto", height: "300px", padding: "10px"}}>
 								<Line data={graph2.data} legend={graph2.legend} options={graph2.options}/>
 							</div>
 						</Grid>
 						<Grid item xs={3}>
-							<div style={{width: "100%", marginTop: "00px", padding: "10px"}}>
+							<div style={{width: "100%", marginTop: "20px", padding: "10px"}}>
 								<ReactTabulator
-									data={table2.data} layout={"fitColumns"} columns={table2.columns}
+									data={table2.data} layout={"fitColumns"} columns={table2.columns} options={tabulator_options}
 									rowClick={table2.rowClick} rowFormatter={table2.rowFormatter} tooltips={true}
 								/>
 							</div>
--- src/public/pd_sigmamu.js
+++ src/public/pd_sigmamu.js
@@ -1,22 +1,19 @@
 /*eslint-env es6*/
-
 import {uncmin} from "numeric";
 
-//let apiresults = jsonfile.results;
-let apiresults = null;
+export const calculateMuSigmaEff = (apiresults) => {
+	let optimizationFunction = function (x) {
+		return computeScenario(x[0], x[1], apiresults.futuresData);
+	};
 
-let optimizationFunction = function (x) {
-	return computeScenario(x[0], x[1], apiresults.futuresData);
+	// debug
+	//let solution = [apiresults.previousSolution.mu, apiresults.previousSolution.sigma];
+	let solution = uncmin(optimizationFunction, [apiresults.previousSolution.mu, apiresults.previousSolution.sigma]).solution;
+
+	return {mu: solution[0], sigma: solution[1]};
 };
 
-let solution = uncmin(optimizationFunction, [apiresults.previousSolution.mu, apiresults.previousSolution.sigma]).solution;
-let mu = solution[0];
-let sigma = solution[1];
-
-console.log(mu);
-console.log(sigma);
-
-export function computeScenario(mu, sig, futuresData) {
+function computeScenario(mu, sig, futuresData) {
 	const BELOW = 6;
 	const ABOVE = 6;
 	const STEP = 10;
@@ -30,13 +27,13 @@ export function computeScenario(mu, sig, futuresData) {
 	let upper = nearest + (ABOVE * STEP);
 	let strike = lower;
 
-	console.log(`price: ${futuresData["price"]}\n`);
-	console.log(`nearest: ${nearest}\n`);
-	console.log(`lower: ${lower}\n`);
-	console.log(`upper: ${upper}\n`);
+	// console.log(`price: ${futuresData["price"]}\n`);
+	// console.log(`nearest: ${nearest}\n`);
+	// console.log(`lower: ${lower}\n`);
+	// console.log(`upper: ${upper}\n`);
 
 	while (strike <= upper) {
-		console.log(` strike: ${strike}\n`);
+		// console.log(` strike: ${strike}\n`);
 
 		let rf = 0.03;
 		let dte = futuresData["dte"];
@@ -44,7 +41,7 @@ export function computeScenario(mu, sig, futuresData) {
 		//******** Call *********
 		let key = `${strike}-0C`;
 		let callPrem = futuresData.data[key];
-		console.log(`callPrem: ${callPrem}\n`);
+		// console.log(`callPrem: ${callPrem}\n`);
 		let callDevSquared = 0;
 		let fk = 0;
 		let iexk = 0;
@@ -59,7 +56,7 @@ export function computeScenario(mu, sig, futuresData) {
 			let callImpliedPrice = (optionBasedEx - iexk - strike / 100 * (1 - fk)) * btCalc;
 
 			callDevSquared = Math.pow(callPrem / 100 - callImpliedPrice, 2);
-			console.log(`callDevSquared: ${callDevSquared}\n`);
+			// console.log(`callDevSquared: ${callDevSquared}\n`);
 		}
 
 		//******** Put *********
@@ -78,14 +75,12 @@ export function computeScenario(mu, sig, futuresData) {
 		cErrors += callDevSquared;
 		pErrors += putDevSquared;
 	}
-
-	let totalErrors = cErrors + pErrors;
-	console.log(`errors: ${totalErrors}\n`);
-
-	return totalErrors;
+	// totalErrors = cErrors + pErrors;
+	// console.log(`errors: ${totalErrors}\n`);
+	return cErrors + pErrors;
 }
 
-export function normalDistribution(z) {
+function normalDistribution(z) {
 
 	let c1 = 2.506628;
 	let c2 = 0.3193815;
@@ -103,56 +98,4 @@ export function normalDistribution(z) {
 	let y = 1 / (1 + 0.2316419 * w * z);
 	return 0.5 + w * (0.5 - (Math.exp(-z * z / 2) / c1) *
 		(y * (c2 + y * (c3 + y * (c4 + y * (c5 + y * c6))))));
-}
-
-export function normsInv(p) {
-	if (typeof p == "string" || p instanceof String) {
-		p = parseFloat(p);
-	}
-	let a1 = -39.6968;
-	let a2 = 220.9461;
-	let a3 = -275.92851;
-	let a4 = 138.3578;
-	let a5 = -30.6648;
-	let a6 = 2.5066;
-	let b1 = -54.47610;
-	let b2 = 161.5858;
-	let b3 = -155.6990;
-	let b4 = 66.8013;
-	let b5 = -13.28068;
-	let c1 = -.00778;
-	let c2 = -0.3224;
-	let c3 = -2.4008;
-	let c4 = -2.5497;
-	let c5 = 4.37466;
-	let c6 = 2.93816;
-	let d1 = .0077847;
-	let d2 = 0.3225;
-	let d3 = 2.4451;
-	let d4 = 3.7544;
-	let p_low = 0.02425;
-	let p_high = 1 - p_low;
-	let q = 0.0;
-	let r = 0.0;
-	let NormSInv = 0;
-	if (p < 0 || p > 1) {
-		return null;
-	}
-	else if (p < p_low) {
-		q = Math.sqrt(-2 * Math.log(p));
-		NormSInv = (((((c1 * q + c2) * q + c3) * q + c4) * q + c5) * q + c6) / ((((d1 * q + d2) * q + d3) *
-			q + d4) * q + 1);
-	}
-	else if (p <= p_high) {
-		q = p - 0.5;
-		r = q * q;
-		NormSInv = (((((a1 * r + a2) * r + a3) * r + a4) * r + a5) * r + a6) * q / (((((b1 * r + b2) *
-			r + b3) * r + b4) * r + b5) * r + 1);
-	}
-	else {
-		q = Math.sqrt(-2 * Math.log(1 - p));
-		NormSInv = -(((((c1 * q + c2) * q + c3) * q + c4) * q + c5) * q + c6) / ((((d1 * q + d2) * q + d3) *
-			q + d4) * q + 1);
-	}
-	return NormSInv;
 }
