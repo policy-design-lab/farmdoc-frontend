c98426ca077059e9b757371d9817fd49ccd4d951
Gowtham Naraharisetty <vnarah2@illinois.edu>
2022-01-14 16:17:03 -0600

    FD-493: Show no FSA data error message

--- src/app.config.js
+++ src/app.config.js
@@ -6,7 +6,7 @@ const farmdocApps = {
 		appDesc: "The Gardner ARC/PLC Calculator shows the likelihood of ARC-CO and" +
 				" PLC making payments in each year from 2019 to 2023. Expected payment " +
 				"levels also are given for user-selected counties and crops.",
-		lastUpdated: "Jan 5, 2021",
+		lastUpdated: "Jan 18, 2022",
 		urlPath: "/arcplc",
 		needsAuthentication: true
 	},
