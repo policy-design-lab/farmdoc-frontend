688cfa578b779dbc947177fc1cd48bf58257e99c
ncsa-mo <ondrejce@illinois.edu>
2022-02-03 08:16:38 -0600

    remove semicolon typo

--- src/components/EvaluatorRiskResults.jsx
+++ src/components/EvaluatorRiskResults.jsx
@@ -565,7 +565,7 @@ class EvaluatorRiskResults extends Component {
 								Farm TA Yield (bu/acre): <span style={{fontWeight: 700}}>{roundResults(farmInfo["trend-adj-aph"], 2)}</span><br />
 								{futuresDate} Futures Price: <span style={{fontWeight: 700}}>${roundResults(farmInfo["avg-futures-price"], 2)}</span><br />
 								Estimated Projected Price: <span style={{fontWeight: 700}}>${roundResults(farmInfo["proj-price"], 2)}</span>
-							</div>;
+							</div>
 						</Grid>
 					</Grid>
 
